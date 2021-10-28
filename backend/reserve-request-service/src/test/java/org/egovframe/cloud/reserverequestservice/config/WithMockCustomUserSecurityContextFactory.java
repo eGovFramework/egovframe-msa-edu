8d86b735d199d5ef8aef75ccfd06e88995e529ac
shinmj <gkdlqkek@gmail.com>
2021-10-28 12:56:32 +0900

    ✅ update test file (reserve-request-service)

---
+++ backend/reserve-request-service/src/test/java/org/egovframe/cloud/reserverequestservice/config/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,25 @@
+package org.egovframe.cloud.reserverequestservice.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithCustomMockUser mockUser) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        List<SimpleGrantedAuthority> roleList = new ArrayList<>();
+        roleList.add(new SimpleGrantedAuthority(mockUser.role().getKey()));
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(mockUser.userId(), null, roleList);
+        context.setAuthentication(authenticationToken);
+
+        return context;
+    }
+}
