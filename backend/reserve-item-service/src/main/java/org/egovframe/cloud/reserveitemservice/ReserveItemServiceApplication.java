f35941d6fd7b8f3f47e78e417a06b2695b95365b
egovframesupport <egovframesupport@gmail.com>
2021-11-08 13:15:18 +0900

    blocking code deps remove

--- backend/reserve-item-service/src/main/java/org/egovframe/cloud/reserveitemservice/ReserveItemServiceApplication.java
+++ backend/reserve-item-service/src/main/java/org/egovframe/cloud/reserveitemservice/ReserveItemServiceApplication.java
@@ -22,17 +22,6 @@ public class ReserveItemServiceApplication {
         String property = Security.getProperty("jdk.tls.disabledAlgorithms").replace(", TLSv1", "").replace(", TLSv1.1", "");
         Security.setProperty("jdk.tls.disabledAlgorithms", property);
 
-        //blocking 코드 감지
-//        BlockHound.builder()
-//                /**
-//                 * mysql r2dbc 에서 호출되는 FileInputStream.readBytes() 가 블로킹코드인데 이를 허용해주도록 한다.
-//                 * 해당 코드가 어디서 호출되는지 알지 못하는 상태에서 FileInputStream.readBytes() 자체를 허용해주는 것은 좋지 않다.
-//                 * 누군가 무분별하게 사용하게 되면 검출해 낼 수 없어 시스템의 위험요소로 남게 된다.
-//                 * r2dbc를 사용하기 위해 FileInputStream.readBytes()를 호출하는 부분만 허용하고 나머지는 여전히 검출대상으로 남기도록 한다.
-//                 */
-//                .allowBlockingCallsInside("dev.miku.r2dbc.mysql.client.ReactorNettyClient", "init")
-//                .install();
-
         SpringApplication.run(ReserveItemServiceApplication.class, args);
     }
 
