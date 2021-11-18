1550c1a5c2d559a7e7818f7e4c875bfd2b2a9d79
shinmj <gkdlqkek@gmail.com>
2021-11-18 16:43:06 +0900

    random 생성시 특수문자제외

--- backend/portal-service/src/main/java/org/egovframe/cloud/portalservice/utils/PortalUtils.java
+++ backend/portal-service/src/main/java/org/egovframe/cloud/portalservice/utils/PortalUtils.java
@@ -75,8 +75,7 @@ public class PortalUtils {
         char[] charSet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
             'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
             'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@',
-            '#', '$', '%', '^', '&'};
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
         StringBuffer sb = new StringBuffer();
 		SecureRandom sr = new SecureRandom();
